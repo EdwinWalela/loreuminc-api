12af1f9a4ee5612774fc58be6a882d95b2156e40
edwinwalela <edwinwalela@gmail.com>
2023-01-30 10:06:56 +0300

    allow cors

--- api/src/app.ts
+++ api/src/app.ts
@@ -3,24 +3,26 @@ import express from 'express';
 import config from './config/config';
 import { exit } from 'process';
 import db from './config/db';
+import cors from 'cors';
 
-import authRoutes from './routes/auth'
-import userRoutes from './routes/users'
+import authRoutes from './routes/auth';
+import userRoutes from './routes/users';
 
 const app: Express = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use('/api/v1/auth',authRoutes);
-app.use('/api/v1/users',userRoutes);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/users', userRoutes);
 
 (async () => {
 	try {
 		await db.connect();
 		console.log('Connected to database');
 	} catch (error) {
-		console.log( 'failed to connect to database', error);
+		console.log('failed to connect to database', error);
 		exit(1);
 	}
 })();
--- api/src/repositories/user.ts
+++ api/src/repositories/user.ts
@@ -1,71 +1,79 @@
-import db from '../config/db'
-import User from '../models/User'
+import db from '../config/db';
+import User from '../models/User';
 
-const createUserQuery = `INSERT INTO "User"(email, password) VALUES ($1, $2) RETURNING id;`
-const getUserByEmailQuery = `SELECT * FROM "User" WHERE email = $1;`
-const getAllUsersQuery = `SELECT email,name,bio,occupation from "User";`
-const getUserByIdQuery = `SELECT email,name,bio,occupation from "User" WHERE id = $1; `
+const createUserQuery = `INSERT INTO "User"(email, password) VALUES ($1, $2) RETURNING id;`;
+const getUserByEmailQuery = `SELECT * FROM "User" WHERE email = $1;`;
+const getAllUsersQuery = `SELECT email,name,bio,occupation,id from "User";`;
+const getUserByIdQuery = `SELECT email,name,bio,occupation,id from "User" WHERE id = $1; `;
 
-const createUser = async (user:User):Promise<number>=>{
-  let res;
-  try {
-    res = await db.query(createUserQuery, [user.email, user.password]);
-  } catch (error:any) {
-    throw new Error(error.message);
-  }
-  return res.rows[0]?.id;
-}
+const createUser = async (user: User): Promise<number> => {
+	let res;
+	try {
+		res = await db.query(createUserQuery, [user.email, user.password]);
+	} catch (error: any) {
+		throw new Error(error.message);
+	}
+	return res.rows[0]?.id;
+};
 
-const getAllUsers = async():Promise<User[]>=>{
-  let res;
-  let users:User[] = [];
+const getAllUsers = async (): Promise<User[]> => {
+	let res;
+	let users: User[] = [];
 
-  try{
-    res= await db.query(getAllUsersQuery);
-  }catch(error:any){
-    throw new Error(error.message)
-  }
-  for (const row of res.rows) {
-    let user = new User(row.email,row.name,row.occupation,row.bio,'',row.id);
-    delete user.password
-    users.push(user)
-  }
-  return users
-}
-const getUserById = async(id:number):Promise<User>=>{
-  let res;
+	try {
+		res = await db.query(getAllUsersQuery);
+	} catch (error: any) {
+		throw new Error(error.message);
+	}
+	for (const row of res.rows) {
+		let user = new User(row.email, row.name, row.occupation, row.bio, '', row.id);
+		delete user.password;
+		users.push(user);
+	}
+	return users;
+};
+const getUserById = async (id: number): Promise<User> => {
+	let res;
 
-  try{
-    res= await db.query(getUserByIdQuery,[id]);
-  }catch(error:any){
-    throw new Error(error.message)
-  }
-  if(res.rowCount == 0){
-    throw new Error('User not found')
-  }
-  let user = new User(res.rows[0].email,res.rows[0].name,res.rows[0].occupation,res.rows[0].bio,'',res.rows[0].id);
-  delete user.password;
-  return user;
-}
+	try {
+		res = await db.query(getUserByIdQuery, [id]);
+	} catch (error: any) {
+		throw new Error(error.message);
+	}
+	if (res.rowCount == 0) {
+		throw new Error('User not found');
+	}
+	let user = new User(
+		res.rows[0].email,
+		res.rows[0].name,
+		res.rows[0].occupation,
+		res.rows[0].bio,
+		'',
+		res.rows[0].id
+	);
+	delete user.password;
+	return user;
+};
 
+const getUserByEmail = async (email: string): Promise<User> => {
+	let res;
+	try {
+		res = await db.query(getUserByEmailQuery, [email]);
+	} catch (error: any) {
+		console.error('Failed to get user by email', error);
+		throw new Error(error.message);
+	}
 
-const getUserByEmail = async (email:string):Promise<User>=>{
-  let res;
-  try {
-    res = await db.query(getUserByEmailQuery, [email]);
-  } catch (error:any) {
-    console.error('Failed to get user by email', error);
-    throw new Error(error.message);
-  }
+	if (res.rowCount === 0) throw new Error('User not found');
 
-  if(res.rowCount === 0) throw new Error('User not found');
+	return new User(
+		res.rows[0]?.email,
+		res.rows[0]?.name,
+		res.rows[0]?.occupation,
+		res.rows[0].bio,
+		res.rows[0]?.password,
+		res.rows[0]?.id
+	);
+};
 
-  return new User(res.rows[0]?.email,
-    res.rows[0]?.name,
-    res.rows[0]?.occupation,
-    res.rows[0].bio,
-    res.rows[0]?.password,
-    res.rows[0]?.id)
-}
-
-export default {createUser,getUserByEmail,getAllUsers,getUserById}
+export default { createUser, getUserByEmail, getAllUsers, getUserById };
